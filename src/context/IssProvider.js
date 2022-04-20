12d97fa41a724d940eab0829c71f2cb81281fe35
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-04-20 15:24:19 -0300

    Requisitos 3 e 4 finalizados com muito suor

--- src/context/IssProvider.js
+++ src/context/IssProvider.js
@@ -3,48 +3,87 @@ import PropTypes from 'prop-types';
 import IssContext from './IssContext';
 
 function IssProvider({ children }) {
-  // É toda a informação da requisição.
-  const [data, setData] = useState([]);
-
-  // O requisito pediu dessa forma.
-  const [nameFilter, setName] = useState(
-    {
-      filterByName: {
-        name: '',
-      },
+  // Requisito 2: usado no filtro do name do planeta.
+  const [planets, setPlanets] = useState([]);
+  // Requisito 2: requisito pediu dessa forma.
+  const nome = {
+    filterByName: {
+      name: '',
     },
-  );
+  };
+  const [nameFilter, setName] = useState(nome);
+  // usado na função da renderização de todo o conteúdo da requisição.
+  const [filterPlanets, setFilterPlanets] = useState([]);
+  // Requisito 2: função que filtra os planetas pelo name. O include determina se no array tem um elemento com o name digitado no input. Ele retorna um booleano. Se returnar true, fará um filter. Dessa forma, atualizará o filterPlanets.
+  const filteredName = (name) => {
+    setFilterPlanets(planets.filter((el) => el.name.includes(name)));
+  };
+  // Reuqisito 3: Esse array vazio receberá todo o conjunto de filtro.
   const [filterValues, setfilterValues] = useState([]);
-  const [filteredPlanet, setFilteredPlanet] = useState([]);
   const [columnFilter, setColumnFilter] = useState('population');
   const [comparationFilter, setComparationFilter] = useState('maior que');
   const [numberFilter, setNumberFilter] = useState(0);
+  // A função vai buscar filtrae se a comparação for "maior que"; se não achar, vai buscar se tiver "menor que"; se não achar, vai buscar "igual que". Sempre deve começar do maior para o menor. Se não tiver selecionada nenhuma dessas opções de comparasion, vai retornar o seu estado inicial, que é um array vazio. No final, inclui na função o filtro do nome dos planetas.
+  const calculation = (objeto) => {
+    const estadoInicial = [];
+    const comparacao = objeto[0].comparison;
+    const coluna = objeto[0].column;
+    const valor = objeto[0].value;
+    const filtro = filterPlanets.filter((element) => {
+      if (comparacao === 'maior que') {
+        return element[coluna] > Number(valor);
+      } if (comparacao === 'menor que') {
+        return element[coluna] < Number(valor);
+      } if (comparacao === 'igual a') {
+        return element[coluna] === (valor);
+      }
+      return estadoInicial;
+    });
+    setFilterPlanets(filtro);
+  };
+  // Quando clicar no button, vai serr renderizado todo o conteúdo do filtro que foi feito a partir da função calculation. Parece que precisa dessa objeto para o próximo requisito, eu acho.
+  const handleClick = () => {
+    const object = {
+      column: columnFilter,
+      comparison: comparationFilter,
+      value: numberFilter,
+    };
+    calculation([...filterValues, object]);
+  };
 
+  // A requisição precisa buscar a informação que tem a chave results. Toda vez que atuliaza a página, o setFilterPlanets recebe o resultado da requisição. Por isso que a página já fica com a tabela montada toda vez que atualiza a página. O setPlanet é quando é digitado o nome do planeta no input.
   const requestApi = async () => {
     const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
     const request = await fetch(endpoint);
-    const response = await request.json();
-    return response.results;
+    const data = await request.json();
+    const response = data.results;
+    setPlanets(response);
+    setFilterPlanets(response);
   };
+
   useEffect(() => {
-    requestApi().then(setData);
+    requestApi();
   }, []);
+
   const values = {
-    data,
-    setData,
+    planets,
     nameFilter,
     setName,
-    filteredPlanet,
-    setFilteredPlanet,
     filterValues,
     setfilterValues,
+    filterPlanets,
+    setFilterPlanets,
+    filteredName,
+    calculation,
     columnFilter,
     setColumnFilter,
     comparationFilter,
     setComparationFilter,
     numberFilter,
     setNumberFilter,
+    handleClick,
   };
+
   return (
     <div>
       <IssContext.Provider value={ values }>
@@ -53,7 +92,9 @@ function IssProvider({ children }) {
     </div>
   );
 }
+
 IssProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
 export default IssProvider;
--- src/pages/Table.js
+++ src/pages/Table.js
@@ -1,119 +1,96 @@
 import React, { useContext } from 'react';
-// import Search from '../componentes/Search';
 import THead from '../componentes/THead';
+import TBody from '../componentes/TBody';
 import IssContext from '../context/IssContext';
 
 function Table() {
   const option = ['population', 'orbital_period',
     'diameter', 'rotation_period', 'surface_water'];
   const {
-    data,
     nameFilter,
-    setName,
-    // columnFilter,
-    // setColumnFilter,
-    // comparationFilter,
-    // setComparationFilter,
-    // numberFilter,
-    // setNumberFilter,
+    filteredName,
     columnFilter,
     setColumnFilter,
     comparationFilter,
     setComparationFilter,
     numberFilter,
     setNumberFilter,
+    handleClick,
   } = useContext(IssContext);
-  console.log(data);
-  // Desconstruir o name que contém o texto digitado no input.
-  const { filterByName: { name } } = nameFilter;
-  // Controlar o texto digitado no input.
-  const handleChange = (event) => {
-    setName({
-      filterByName: {
-        name: event.target.value,
-      },
-    });
-  };
+
   return (
     <div>
       <h1>Projeto Star Wars - Trybe</h1>
-      <label htmlFor="nameFilter">
-        Qual planeta você procura?
+      <label
+        htmlFor="nameFilter"
+      >
         <input
           id="nameFilter"
-          name="nameFilter"
+          type="text"
+          name="name"
           data-testid="name-filter"
-          onChange={ handleChange }
+          value={ nameFilter }
+          onChange={ (event) => filteredName(event.target.value) }
         />
       </label>
-      <div>
+
+      <label
+        htmlFor="columnFilter "
+      >
         <select
+          id="columnFilter "
           value={ columnFilter }
-          name="column"
           data-testid="column-filter"
-          // Outra forma de controlar o valor do input
           onChange={ (event) => setColumnFilter(event.target.value) }
         >
           {option.map((element, index) => (
             <option key={ index } value={ element }>{ element }</option>
           ))}
         </select>
+      </label>
+      <label
+        htmlFor="comparationFilter"
+      >
         <select
-          value={ comparationFilter }
-          name="valor"
+          id="comparationFilter"
           data-testid="comparison-filter"
+          value={ comparationFilter }
           onChange={ (event) => setComparationFilter(event.target.value) }
         >
-          <option value="maior que">maior que</option>
-          <option value="menor que">menor que</option>
-          <option value="igual a">igual a</option>
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
         </select>
-        <label
-          htmlFor="valueFilter"
-        >
-          <input
-            value={ numberFilter }
-            id="valueFilter"
-            type="number"
-            data-testid="value-filter"
-            onChange={ (event) => setNumberFilter(event.target.value) }
-          />
-        </label>
-        <button
-          type="button"
-          data-testid="button-filter"
-          // onClick={ handleClick }
-        >
-          Filtrar
-        </button>
-      </div>
+      </label>
+      <label
+        htmlFor="valueFilter"
+      >
+        <input
+          id="valueFilter"
+          data-testid="value-filter"
+          type="number"
+          value={ numberFilter }
+          onChange={ (event) => setNumberFilter(event.target.value) }
+        />
+      </label>
 
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleClick }
+      >
+        Filtrar
+      </button>
       <table>
         <thead>
           <THead />
         </thead>
-        <tbody>
-          {data.filter((el) => el.name.includes(name))
-            .map((element) => (
-              <tr key={ element.name }>
-                <td>{element.name}</td>
-                <td>{element.rotation_period}</td>
-                <td>{element.orbital_period}</td>
-                <td>{element.diameter}</td>
-                <td>{element.climate}</td>
-                <td>{element.gravity}</td>
-                <td>{element.terrain}</td>
-                <td>{element.surface_water}</td>
-                <td>{element.population}</td>
-                <td>{element.films}</td>
-                <td>{element.created}</td>
-                <td>{element.edited}</td>
-                <td>{element.url}</td>
-              </tr>
-            ))}
-        </tbody>
+        <thbody>
+          <TBody />
+        </thbody>
       </table>
     </div>
   );
 }
+
 export default Table;
